f82c45169f215e5aaea7e54e2f844961fccec3d8
nitish-k00 <nitish.k2021@vitbhopal.ac.in>
2024-06-01 12:15:32 +0530

    data persist LocalStorage

--- Tic Tac Toe/src/TTT.js
+++ Tic Tac Toe/src/TTT.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de61d0b0bdd74f5146f6a00260daaeca0db1795f19b1600c3254f48830ca3339
-size 3585
+oid sha256:b5dd9fab7682e69ec02671ee0cc7560d99035927c0667674b3bd637e158477f9
+size 4564
