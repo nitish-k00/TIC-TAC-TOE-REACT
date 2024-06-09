1e0785d517ed3acb90cb5917df03d483170982d7
nitish-k00 <nitish.k2021@vitbhopal.ac.in>
2024-06-10 02:54:04 +0530

    added total win count

--- Tic Tac Toe/src/TTT.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b5dd9fab7682e69ec02671ee0cc7560d99035927c0667674b3bd637e158477f9
-size 4564
---
+++ TicTacToe/src/TTT.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70cd9ca7dbc1fc6a6d0843e66efa045d33db15418a789ba8659bae18b4ef8f78
+size 6189
